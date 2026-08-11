69466ced1091871862a2024b1740a454d2fb7d85
Andi Ade Indawan <andiadeindawan@gmail.com>
2026-08-11 15:23:07 +0800

    update

--- resources/views/layouts/peserta.blade.php
+++ resources/views/layouts/peserta.blade.php
@@ -567,6 +567,9 @@
                 <a href="{{ route('peserta.sertifikat.index') }}" class="nav-link {{ request()->routeIs('peserta.sertifikat.*') ? 'active' : '' }}">
                     Sertifikat
                 </a>
+                 <a href="{{ route('peserta.dokumentasi.index') }}" class="nav-link {{ request()->routeIs('peserta.dokumentasi.index') ? 'active' : '' }}">
+                    Dokumentasi
+                </a>
             </div>
 
             <!-- ========================================================== -->
@@ -580,9 +583,6 @@
             
             <a href="{{ route('peserta.pengumuman.index') }}" class="nav-link {{ request()->routeIs('peserta.pengumuman.*') ? 'active' : '' }}">
                 <i class="bi bi-megaphone"></i> Pengumuman
-            </a>
-            <a href="{{ route('peserta.dokumentasi.index') }}" class="nav-link {{ request()->routeIs('peserta.dokumentasi.index') ? 'active' : '' }}">
-                    <i class="bi bi-images me-2"></i>Dokumentasi
             </a>
             <a href="{{ route('peserta.survey.index') }}" class="nav-link {{ request()->routeIs('peserta.survey.*') ? 'active' : '' }}">
                 <i class="bi bi-ui-radios me-2"></i>Survey
